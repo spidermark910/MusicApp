ca8ef927bc26ab3e442395dd105ffad1ed330b50
spidermark910 <spidermark910@gmail.com>
2023-05-17 22:40:42 -0500

    Init Vue Project

---
+++ MusicApplication/src/views/ProductsView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="products">
+    <h1>This is the products page.</h1>
+    <p>Look at all the products here!</p>
+  </div>
+</template>
+
+<style>
+p {
+  color: red;
+}
+
+@media (min-width: 1024px) {
+  .products {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
